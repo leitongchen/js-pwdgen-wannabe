8e5c2ae7096bd6e56606d4de5295ee24096df561
leitongchen <leitongchen93@gmail.com>
2021-04-07 16:34:09 +0200

    aggiunto style

--- myscript.js
+++ myscript.js
@@ -1,12 +1,12 @@
-var nome = prompt("Come ti chiami?");
+var yourName = prompt("Come ti chiami?");
 
-var cognome = prompt("Qual è il tuo cognome?");
+var yourSurname = prompt("Qual è il tuo cognome?");
 
-var colorePreferito = prompt("Qual è il tuo colore preferito?");
+var yourFavColour = prompt("Qual è il tuo colore preferito?");
 
 
-console.log(nome + cognome + colorePreferito + 21);
+console.log(yourName + yourSurname + yourFavColour + 21);
 
-var myPassword = nome + cognome + colorePreferito + 21;
+var myPassword = yourName + yourSurname + yourFavColour + 21;
 
-document.getElementById("password-container").innerHTML = "Questa è la tua nuova password <br> <strong>" + myPassword + "</strong> <br> Conservala con cura."
+document.getElementById("pw-gen").innerHTML = myPassword;
